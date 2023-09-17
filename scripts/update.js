2e6d91f551265311c5b9d2f59a86f4e444aec0e0
Julien Moreau <julien@thylo.be>
2023-09-17 21:01:15 +0200

    init

---
+++ scripts/update.js
@@ -0,0 +1,15 @@
+import { exec } from "child_process";
+
+exec("git pull", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+
+  console.log(`stdout:\n${stdout}`);
+});
---
+++ server.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.listen(port, () => {});
