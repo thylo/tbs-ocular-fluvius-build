8939b8b4958977f34ef7d959833bc0a1d7aeb174
Julien Moreau <julien@thylo.be>
2023-09-18 11:13:46 +0200

    sources

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-import open from "open";
+import open, {apps} from "open";
 
 app.use(express.static("public"));
 
 app.listen(port, async () => {
   await open(`http://localhost:${port}/#/${process.env.GAME_ID}`, {
-    app: { name: "google chrome", arguments: ["--kiosk"] },
+    app: { name: apps.chrome, arguments: ["--kiosk", "--disable-pinch", "--overscroll-history-navigation=0", "--allow-file-access-from-files", "--check-for-update-interval=604800"] },
   });
   console.log(`Opening app http://localhost:${port}/#/${process.env.GAME_ID}`);
 });
