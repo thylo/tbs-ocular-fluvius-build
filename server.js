9d57cb9f2734715f3366b5e73179967cda83c971
Julien Moreau <julien@thylo.be>
2023-09-18 10:15:28 +0200

    open browser

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import express from "express";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+import open from "open";
 
-app.listen(port, () => {});
+app.use(express.static("public"));
+
+app.listen(port, async () => {
+  await open(`http://localhost:${port}/#/${process.env.GAME_ID}`, {
+    app: { name: "google chrome", arguments: ["--kiosk"] },
+  });
+  console.log(`Opening app http://localhost:${port}/#/${process.env.GAME_ID}`);
+});
